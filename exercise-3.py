1e134799b016a1b5309ad9259636409d60a257ee
Nurai19 <93372650+Nurai19@users.noreply.github.com>
2021-11-29 23:45:56 +0600

    Update exercise-3.py

---
+++ exercise-3.py
@@ -0,0 +1,23 @@
+# 1
+# func = ['hello', 5, ]
+# func.append('element')
+# func.insert(0,'start')
+# func.append('finish')
+# print(func)
+#
+# 2 Не поняла как делать(
+
+# 3
+# tuple_numbers = (1, 2, 3, 4, 5)
+# lst = list(tuple_numbers)
+# odd_numbers = []
+# even_numbers = []
+#
+#
+# for i in lst:
+#     if i % 2 == 0:
+#         even_numbers.append(i)
+#     else:
+#         odd_numbers.append(i)
+# print(f'Список четных чисел: {even_numbers}')
+# print(f'Список нечетных чисел: {odd_numbers}')
