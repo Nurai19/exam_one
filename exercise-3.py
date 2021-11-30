5adeba01633224c5ff33bfcdef96a03010b006f6
Nurai19 <93372650+Nurai19@users.noreply.github.com>
2021-11-30 23:01:25 +0600

    Update exercise-3.py

--- exercise-3.py
+++ exercise-3.py
@@ -1,23 +1,30 @@
 # 1
-# func = ['hello', 5, ]
-# func.append('element')
-# func.insert(0,'start')
-# func.append('finish')
-# print(func)
+# def func(list_: list) -> list:
+#     list_.append('element')
+#     list_.insert(0, 'start')
+#     list_.append('finish')
 #
+#     return list_
+#
+#
+# print(func(['hello', 5, 'John']))
+
 # 2 Не поняла как делать(
 
 # 3
-# tuple_numbers = (1, 2, 3, 4, 5)
-# lst = list(tuple_numbers)
-# odd_numbers = []
-# even_numbers = []
-#
-#
-# for i in lst:
-#     if i % 2 == 0:
-#         even_numbers.append(i)
-#     else:
-#         odd_numbers.append(i)
-# print(f'Список четных чисел: {even_numbers}')
-# print(f'Список нечетных чисел: {odd_numbers}')
+# def func(tuple_):
+# 
+#     x = list(tuple_)
+#     odd_numbers = []
+#     even_numbers = []
+# 
+#     for i in x:
+#         if i % 2 == 0:
+#             even_numbers.append(i)
+#         else:
+#             odd_numbers.append(i)
+#     return even_numbers, odd_numbers
+# 
+# 
+# a, b = func((1, 2, 3, 4, 5))
+# print(a, b)
